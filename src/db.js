33f8cb5a612116a5a07f2d3b50fd3b52596060c2
Daniel Perco <danielperco4q@gmail.com>
2022-11-06 19:21:52 -0300

    back1

--- src/db.js
+++ src/db.js
@@ -3,14 +3,14 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const { deprecate } = require('util');  
-// const {
-//   DB_USER, DB_PASSWORD, DB_HOST,
-// } = process.env;
+ const {
+  DB_USER, DB_PASSWORD, DB_HOST,
+ } = process.env;
 
- const sequelize = new Sequelize(`postgres://postgres:123456@localhost/pokemon`, {
-  logging: false, // set to console.log to see the raw SQL queries
-   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
- });
+//  const sequelize = new Sequelize(`postgres://postgres:123456@localhost/pokemon`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//  });
  const basename = path.basename(__filename);
 
  const modelDefiners = [];  
